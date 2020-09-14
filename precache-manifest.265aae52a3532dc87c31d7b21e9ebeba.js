7e72b6c3b1529938291fdfcce5f514a7d62b2f15
mahejon <jmaher1@gmail.com>
2020-09-14 15:36:22 -0400

    Updates

---
+++ precache-manifest.265aae52a3532dc87c31d7b21e9ebeba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f03563f9fc61d585dec927b390e167a",
+    "url": "/react-tuts/index.html"
+  },
+  {
+    "revision": "10805549c9d2afc2aba8",
+    "url": "/react-tuts/static/css/main.1b740a84.chunk.css"
+  },
+  {
+    "revision": "0c93387514e954bdf57e",
+    "url": "/react-tuts/static/js/2.605ea4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tuts/static/js/2.605ea4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10805549c9d2afc2aba8",
+    "url": "/react-tuts/static/js/main.ffac8c5f.chunk.js"
+  },
+  {
+    "revision": "ce3e4c55d5f827dada8e",
+    "url": "/react-tuts/static/js/runtime-main.39d47a0f.js"
+  }
+]);
